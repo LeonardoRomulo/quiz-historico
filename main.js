f99ab1deceafd4707c94a99e31aa4ded26d2731a
LeonardoRomulo <leo.romulo15@gmail.com>
2025-03-14 10:54:49 -0300

    feat: atualização na exibição de pontuação e o nome do jogador no final da execução no terminal

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ import { exibirQuestoes, exibirResultado } from './functions/displayToUser.js';
 console.log("Seja Bem-vindo jogador(a)!");
 const nome = entradaDados.question("Digite o seu nome: ");
 
-
 const quantidadePerguntas = 10;
-const perguntasSelecionadas = selecionarQuestoesAleatorias(questoes, quantidadePerguntas)
+const perguntasSelecionadas = selecionarQuestoesAleatorias(questoes, quantidadePerguntas);
 const respostasCorretas = exibirQuestoes(perguntasSelecionadas);
 exibirResultado(respostasCorretas);
+console.log(`Jogador(a): ${nome}`);
+console.log(`Pontuação final: ${respostasCorretas}`)
 
 export { embaralharQuestoes, selecionarQuestoesAleatorias, exibirQuestoes };
